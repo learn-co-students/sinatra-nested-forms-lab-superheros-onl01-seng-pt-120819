28f698773c62b300d702d8eecb07ebb327a8feab
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-15 01:02:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
 
+        erb :team
+    end
+
+    post '/teams' do 
+        puts params
+        erb :super_hero
+    end
 end
